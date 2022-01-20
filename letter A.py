6a32b92c0080297f46625fc6a3cf93f10754d613
am3357 <97529621+am3357@users.noreply.github.com>
2022-01-20 18:03:11 +0000

    Updating
    adding more letters

--- letter A.py
+++ letter A.py
@@ -55,9 +55,60 @@ client.put_pixels(leds)
 leds[310]=(0,0,255)
 time.sleep(0.1)
 client.put_pixels(leds)
+#A
 
+leds[15]=(0,0,255)
+leds[75]=(0,0,255)
+leds[135]=(0,0,255)
+leds[195]=(0,0,255)
+leds[255]=(0,0,255)
+leds[315]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
 
+leds[16]=(0,0,255)
+leds[316]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
 
+leds[17]=(0,0,255)
+leds[317]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
 
+leds[18]=(0,0,255)
+leds[318]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
 
+leds[19]=(0,0,255)
+leds[319]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
 
+leds[80]=(0,0,255)
+leds[260]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[141]=(0,0,255)
+leds[201]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+#D
+leds[326]=(0,0,255)
+time.sleep(2)
+client.put_pixels(leds)
+
+leds[267]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[208]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[149]=(0,0,255)
+leds[209]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
