f487f311dda9062a632569ccbb381d39ce16960d
am3357 <97529621+am3357@users.noreply.github.com>
2022-01-22 18:57:57 +0000

    Update letter A.py

--- letter A.py
+++ letter A.py
@@ -4,7 +4,7 @@ import random
 
 client = opc.Client('localhost:7890')
 
-leds = [(255,255,255)]*360 #white background
+leds = [(0,0,0)]*360 #white background
 client.put_pixels(leds)
 
 leds[300]=(0,0,255)
@@ -55,7 +55,6 @@ client.put_pixels(leds)
 leds[310]=(0,0,255)
 time.sleep(0.1)
 client.put_pixels(leds)
-#A
 
 leds[15]=(0,0,255)
 leds[75]=(0,0,255)
@@ -95,9 +94,9 @@ leds[141]=(0,0,255)
 leds[201]=(0,0,255)
 time.sleep(0.1)
 client.put_pixels(leds)
-#D
+
 leds[326]=(0,0,255)
-time.sleep(2)
+time.sleep(0.1)
 client.put_pixels(leds)
 
 leds[267]=(0,0,255)
@@ -112,3 +111,108 @@ leds[149]=(0,0,255)
 leds[209]=(0,0,255)
 time.sleep(0.1)
 client.put_pixels(leds)
+
+leds[90]=(0,0,255)
+leds[210]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[31]=(0,0,255)
+leds[211]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[92]=(0,0,255)
+leds[212]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[153]=(0,0,255)
+leds[213]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[214]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[275]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[336]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[341]=(0,0,255)
+leds[281]=(0,0,255)
+leds[221]=(0,0,255)
+leds[161]=(0,0,255)
+leds[101]=(0,0,255)
+leds[41]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[102]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[163]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[224]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[285]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[346]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[287]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[228]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[169]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[110]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[51]=(0,0,255)
+leds[111]=(0,0,255)
+leds[171]=(0,0,255)
+leds[231]=(0,0,255)
+leds[291]=(0,0,255)
+leds[351]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[54]=(0,0,255)
+leds[114]=(0,0,255)
+leds[174]=(0,0,255)
+leds[234]=(0,0,255)
+leds[354]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+leds[57]=(0,0,255)
+leds[117]=(0,0,255)
+leds[177]=(0,0,255)
+leds[237]=(0,0,255)
+leds[357]=(0,0,255)
+time.sleep(0.1)
+client.put_pixels(leds)
+
+#code up to here plays ADAM!! in LEDs
+
