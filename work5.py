f04c498d57465be7a08d6645918e0d8d1d25959f
am3357 <97529621+am3357@users.noreply.github.com>
2022-02-08 23:14:03 +0000

    a few tweaks
    didnt accomplish much physically but i learnt a bit

--- work5.py
+++ work5.py
@@ -8,21 +8,29 @@ leds = [(255,255,255)]*360 #black background
 client.put_pixels(leds)
 
 def less1(k):
-    for x in range(0,k):
+    for x in range(k-1,-1,-1):
         leds[x]=(0,0,255)
-        time.sleep(0.1)
+        time.sleep(0.01)
         client.put_pixels(leds)
-
 def more1(k):
     for x in range(k+1,60):
         leds[x]=(0,0,255)
-        time.sleep(0.1)
+        time.sleep(0.01)
         client.put_pixels(leds)
-def exact1(k):
-    less1(k)
-    time.sleep(0.1)
-    client.put_pixels(leds)
-    more1(k)
-    time.sleep(0.1)
-    client.put_pixels(leds)
-exact1(5)
+#def exact1(k):
+#    less1(k)
+#    time.sleep(0.1)
+#    more1(k)
+#    time.sleep(0.1)
+#    client.put_pixels(leds)
+#exact1(5)
+def exact(k):
+    while True:
+        less1(k)
+        time.sleep(0.01)
+        client.put_pixels(leds)
+        more1(k)
+        time.sleep(0.01)
+        client.put_pixels(leds)
+exact(30)
+
