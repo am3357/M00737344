de00448113b5509300a6e9be8df4ddc1583c0d9b
am3357 <97529621+am3357@users.noreply.github.com>
2022-02-08 18:47:51 +0000

    updated my new file
    added a definition code, for more and less so whichever led i pick i will be able to fill it up from 0 up to it  in the line and from 60 down to it in the line

--- work5.py
+++ work5.py
@@ -7,9 +7,22 @@ client = opc.Client('localhost:7890')
 leds = [(255,255,255)]*360 #black background
 client.put_pixels(leds)
 
-def less(k):
-    for x in range(x>k):
-        leds[k]=(0,0,255)
-        time.sleep(2)
+def less1(k):
+    for x in range(0,k):
+        leds[x]=(0,0,255)
+        time.sleep(0.1)
         client.put_pixels(leds)
-less(30)
+
+def more1(k):
+    for x in range(k+1,60):
+        leds[x]=(0,0,255)
+        time.sleep(0.1)
+        client.put_pixels(leds)
+def exact1(k):
+    less1(k)
+    time.sleep(0.1)
+    client.put_pixels(leds)
+    more1(k)
+    time.sleep(0.1)
+    client.put_pixels(leds)
+exact1(5)
